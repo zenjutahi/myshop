6ef1aa8a46d411938d93defa96fd9c243a11dc58
Zenjutahi <zenjutahi@gmail.com>
2019-08-29 11:48:48 +0300

    [feature] - reset the app

--- orders/models.py
+++ orders/models.py
@@ -26,8 +26,8 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-	order = models.ForeignKey(Order, related_name='items')
-	product = models.ForeignKey(Product, related_name='order_items')
+	order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+	product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')
 	price = models.DecimalField(max_digits=10, decimal_places=2)
 	quantity = models.PositiveIntegerField(default=1)
 
