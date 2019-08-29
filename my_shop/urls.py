dd524947a4f7fa75761732fb2f75b8073dc5972e
Zenjutahi <zenjutahi@gmail.com>
2019-08-29 11:48:36 +0300

    [feature] - reset the app

--- my_shop/urls.py
+++ my_shop/urls.py
@@ -20,9 +20,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^cart/', include('cart.urls', namespace='cart')),
-    url(r'^orders/', include('orders.urls', namespace='orders')),
-    url(r'^', include('shop.urls', namespace='shop')),
+    url(r'^cart/', include(('cart.urls', 'cart'), namespace='cart')),
+    url(r'^orders/', include(('orders.urls', 'orders'), namespace='orders')),
+    url(r'^', include(('shop.urls', 'shop'), namespace='shop')),
 ]
 if settings.DEBUG:
 	urlpatterns += static(settings.MEDIA_URL, 
