6f73c0dc46d56e684fd1e78f30bb9f7986281a9e
zenjutahi <zenjutahi@gmail.com>
2020-02-12 15:08:55 +0300

    [chore] setup for deploy in rehoku

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -143,10 +143,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'myshop/static/'
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-# Extra places for collectstatic to find static files.
+STATIC_ROOT= os.path.join(PROJECT_DIR,'staticfiles/')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'myshop/static'),
+    os.path.join(PROJECT_ROOT,'static/'),
 )
