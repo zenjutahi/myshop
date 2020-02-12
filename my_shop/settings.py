94c2d332beff9503445f8250d80b2f39704d018e
zenjutahi <zenjutahi@gmail.com>
2020-02-12 12:14:26 +0300

    [chore] setup for deploy in rehoku

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# 
+#
 # from celery import Celery
 #
 # celery = Celery(broker="amqp://guest:guest@127.0.0.1:5672//")
@@ -140,5 +140,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
