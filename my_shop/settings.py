f52b8f3a9f4375391258f1513ec3f3b2fe39ff6b
zenjutahi <zenjutahi@gmail.com>
2020-02-12 17:56:01 +0300

    [chore] setup for deploy in heroku

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -144,4 +144,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
