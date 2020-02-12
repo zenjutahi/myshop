bb35b67d4ba1f60089e7732111fb9c99c4832c74
zenjutahi <zenjutahi@gmail.com>
2020-02-12 15:27:23 +0300

    [chore] setup for deploy in heroku

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -150,6 +150,4 @@ STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 STATIC_ROOT= os.path.join(PROJECT_ROOT, 'staticfiles/')
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static/'),
-)
+STATICFILES_DIRS = ()
