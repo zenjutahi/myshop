35a9ef35d6298e8a2a1fd1a8a9939a4f28f173fb
zenjutahi <zenjutahi@gmail.com>
2020-02-15 21:10:42 +0300

    [chore] -reset static files

--- orders/tasks.py
+++ orders/tasks.py
@@ -1,8 +1,8 @@
-# from celery import task
+from celery import task
 from django.core.mail import send_mail
 from .models import Order
 
-# @task
+@task
 def order_created(order_id):
 	"""
 	Task to send an e-mail notification when an order is successfully created
