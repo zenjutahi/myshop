2261eaecae2f52e47443dc3d1dbf8d2c939ba486
zenjutahi <zenjutahi@gmail.com>
2020-02-15 21:11:58 +0300

    [chore] -reset static files

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -144,4 +144,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = '/static/'
